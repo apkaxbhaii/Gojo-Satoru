37fa4727e112f91fde29e02c348484f798f569f0
apkaxbhaii <109059102+apkaxbhaii@users.noreply.github.com>
2022-08-07 22:24:11 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -13,9 +13,10 @@ global.APIs = {
 	zenz: 'https://zenzapis.xyz',
 }
 
+//sanzychan01\\
 //Api Key\\
 global.APIKeys = {
-	'https://zenzapis.xyz': 'sanzychan01', //credit goes to sanzy yt, if api key expire, buy it and put it here?\\
+	'https://zenzapis.xyz': 'b87fd3a987', //credit goes to sanzy yt, if api key expire, buy it and put it here?\\
 }
 
 //customize settings\\
